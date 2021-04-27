060b4e2fa33687d59ee2dead8abe1ed1735e0c8e
jwosborn <osbornjeremyw@yahoo.com>
2021-04-27 15:35:51 -0400

    delete unused files

--- routes/auth.js
+++
@@ -1,7 +0,0 @@
-var express = require("express");
-var router = express.Router();
-
-// router.get("/login", (req, res) => {
-//   const user = req.user;
-//   login(user);
-// });
--- routes/login.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var router = express.Router();
-var user = { email: "user", password: "password" };
-
-router.get("/", (req, res) => {
-  res.json(user);
-});
-
-module.exports = router;
